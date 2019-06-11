a305dd3ec917b7dfbd1d371c49e02129dac6fd1d
mavharsha <sreeharsha.mamilla@gmail.com>
2019-06-11 07:59:19 -0700

    Chapter-3 Working version of Xylophone.

--- xylophone-flutter/lib/main.dart
+++ xylophone-flutter/lib/main.dart
@@ -7,34 +7,12 @@ class XylophoneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blue,
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            musicNote(Colors.blue.shade900, "notes1.wav"),
-            musicNote(Colors.blue.shade800, "notes2.wav"),
-            musicNote(Colors.blue.shade700, "notes3.wav"),
-            musicNote(Colors.blue.shade600, "notes4.wav"),
-            musicNote(Colors.blue.shade500, "notes5.wav"),
-            musicNote(Colors.blue.shade400, "notes6.wav"),
-            musicNote(Colors.blue.shade300, "notes7.wav"),
-          ],
-        ),
-      ),
-    );
-  }
-  musicNote(Color color, String noteName) {
-    return Expanded(
-      child: FlatButton(onPressed: () {
-        final audioPlayer = AudioCache();
-        audioPlayer.play(noteName);
-      },
-        color: color,),
-    );
+        home: Scaffold(
+      backgroundColor: Colors.blue,
+      body: MusicNotes(),
+    ));
   }
 }
-
 
 class MusicNotes extends StatelessWidget {
   @override
@@ -42,23 +20,26 @@ class MusicNotes extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        musicNote(Colors.blue.shade900, ""),
-        musicNote(Colors.blue.shade800, ""),
-        musicNote(Colors.blue.shade700, ""),
-        musicNote(Colors.blue.shade600, ""),
-        musicNote(Colors.blue.shade500, ""),
-        musicNote(Colors.blue.shade400, ""),
-        musicNote(Colors.blue.shade300, ""),
+        musicNote(Colors.blue.shade900, "note1.wav"),
+        musicNote(Colors.blue.shade800, "note2.wav"),
+        musicNote(Colors.blue.shade700, "note3.wav"),
+        musicNote(Colors.blue.shade600, "note4.wav"),
+        musicNote(Colors.blue.shade500, "note5.wav"),
+        musicNote(Colors.blue.shade400, "note6.wav"),
+        musicNote(Colors.blue.shade300, "note7.wav"),
       ],
     );
   }
 
   musicNote(Color color, String noteName) {
     return Expanded(
-      child: FlatButton(onPressed: () {
-
-      },
-      color: color,),
+      child: FlatButton(
+        onPressed: () {
+          final audioPlayer = AudioCache();
+          audioPlayer.play(noteName);
+        },
+        color: color,
+      ),
     );
   }
 }
